2e71de9ccfa4b1506ef6c4d9e3e214adabf33fa2
Muhammed Shan <shanushan73011@gmail.com>
2025-02-17 14:19:14 +0530

    add env

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const http = require('http')
 const app = express()
 const path = require('path')
-const PORT = 3000
+const PORT = process.env.PORT
 const socketio = require('socket.io') 
 const server = http.createServer(app)
 const io = socketio(server)
